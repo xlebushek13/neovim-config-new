b612d273f03aa31cd5c6f66cc6ec39d7d7067f00
xlebushek13 <xlebushekbb@gmail.com>
2024-11-11 19:07:15 +0200

    Markdown

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -14,6 +14,55 @@ return {
 	},
 
 	{
+		"topazape/md-preview.nvim",
+		ft = { "md", "markdown", "mkd", "mkdn", "mdwn", "mdown", "mdtxt", "mdtext", "rmd", "wiki" },
+		config = function()
+			require("md-preview").setup({
+				-- your configuration comes here
+				-- or leave it empty to use the default settings
+				-- refer to the setup section below
+				viewer = {
+					-- mdcat
+					exec = "mdcat",
+					exec_path = "",
+					args = { "--local" },
+
+					-- glow
+					-- exec = "glow",
+					-- exec_path = "",
+					-- args = { "-s", "dark" },
+
+					-- terminal markdown viewer
+					-- exec = "mdv",
+					-- exec_path = "",
+					-- args = { "-c", 80 },
+				},
+				-- Markdown preview term
+				term = {
+					-- reload term when rendered markdown file changed
+					reload = {
+						enable = true,
+						events = { "InsertLeave", "TextChanged" },
+					},
+					direction = "vertical", -- choices: vertical / horizontal
+					keys = {
+						close = { "q" },
+						refresh = "r",
+					},
+				},
+			})
+		end,
+	},
+
+	{
+		"tadmccorkle/markdown.nvim",
+		ft = "markdown", -- or 'event = "VeryLazy"'
+		opts = {
+			-- configuration here or empty for defaults
+		},
+	},
+
+	{
 		"echasnovski/mini.hipatterns",
 		event = "BufReadPre",
 		-- opts = {
