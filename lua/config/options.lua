c366f7714390ea7f30d36d586a66af8c95ab8a3a
xlebushek <xlebushekbb@gmail.com>
2025-12-02 13:40:02 +0100

    Fixes

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -35,7 +35,7 @@ vim.opt.splitbelow = true -- Put new windows below current
 vim.opt.splitright = true -- Put new windows right of current
 vim.opt.splitkeep = "cursor"
 vim.opt.mouse = "a"
-vim.g.autoformat = false
+vim.opt.clipboard = "unnamedplus"
 
 -- Undercurl
 vim.cmd([[let &t_Cs = "\e[4:3m"]])
