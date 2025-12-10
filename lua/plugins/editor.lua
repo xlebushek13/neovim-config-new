828bff8f12684286b84411a444ae610a173a5bf4
xlebushek13 <xlebushekbb@gmail.com>
2025-12-10 18:13:09 +0100

    Change telescope position

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -177,16 +177,6 @@ return {
 			local actions = require("telescope.actions")
 			local fb_actions = require("telescope").extensions.file_browser.actions
 
-			opts.defaults = vim.tbl_deep_extend("force", opts.defaults, {
-				wrap_results = true,
-				layout_strategy = "horizontal",
-				layout_config = { prompt_position = "top" },
-				sorting_strategy = "ascending",
-				winblend = 0,
-				mappings = {
-					n = {},
-				},
-			})
 			opts.pickers = {
 				diagnostics = {
 					theme = "ivy",
